62e06c37044adfe71e600ff4949aececd624adc4
MelinaGiacone <yb7099284@iespoblenou.org>
2024-11-26 09:58:41 +0100

    Add divide method

--- src/Calculator.java
+++ src/Calculator.java
@@ -10,11 +10,22 @@ public class Calculator {
         return a * b;
     }
 
+    public int divide(int a, int b){
+        if ( b == 0){
+            System.out.println("División por cero no está definida matemáticamente");
+            return b;
+        } else {
+            return a / b;
+        }
+    }
+
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
         System.out.println("Addition: " + calculator.add(10, 5));
         System.out.println("Subtraction: " + calculator.subtract(10, 5));
         System.out.println("Multiply: " + calculator.multiply(10, 5));
-}
+        System.out.println("Divide: " + calculator.divide(10, 0));
+
+    }
 }
 
