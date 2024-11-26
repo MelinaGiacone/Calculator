89400d8de77e7a1f080aa6e2a4b377293d4c0e6b
MelinaGiacone <yb7099284@iespoblenou.org>
2024-11-26 09:40:34 +0100

    Add multiply method

--- src/Calculator.java
+++ src/Calculator.java
@@ -5,8 +5,16 @@ public class Calculator {
     public int subtract(int a, int b) {
         return a - b;
     }
+
+    public int multiply(int a, int b){
+        return a * b;
+    }
+
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
         System.out.println("Addition: " + calculator.add(10, 5));
-        System.out.println("Subtraction: " + calculator.subtract(10, 5)); }
+        System.out.println("Subtraction: " + calculator.subtract(10, 5));
+        System.out.println("Multiply: " + calculator.multiply(10, 5));
 }
+}
+
