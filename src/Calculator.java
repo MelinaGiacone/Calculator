88da2a3c3a56486fdd1aa0725aad61f481b72268
MelinaGiacone <yb7099284@iespoblenou.org>
2024-12-03 08:34:12 +0100

    Add Modulus at new branch

--- src/Calculator.java
+++ src/Calculator.java
@@ -19,13 +19,18 @@ public class Calculator {
         }
     }
 
+    public int modulus(int a, int b) {
+            return a % b;
+        }
+
+
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
         System.out.println("Addition: " + calculator.add(10, 5));
         System.out.println("Subtraction: " + calculator.subtract(10, 5));
         System.out.println("Multiply: " + calculator.multiply(10, 5));
         System.out.println("Divide: " + calculator.divide(10, 0));
-
+        System.out.println("Modulus: " + calculator.modulus(10, 0));
     }
 }
 
